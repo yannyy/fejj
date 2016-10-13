43ca83911f9576f033aeafdb521f075e42e6548a
yannyy <yangyan4406@126.com>
2016-10-13 16:30:00 +0800

    fix  scroll problem

--- js/imgscrollview.js
+++ js/imgscrollview.js
@@ -10,20 +10,68 @@
 
 	    var initSelectPoints = function(){
 	    	var pointUl = $(wizard).find('div.dDiv').find('ul');
+	    	$(pointUl).find('li').unbind();
 	    	$(pointUl).find('li').remove();
 
 	    	for(var i = 0; i < wizard.imgNums; i ++){
+	    		var point = null;
 	    		if(i == wizard.current)
-	    			$(pointUl).append('<li class="current"><a href="javascript:;">●</a></li>');
+	    			point = $('<li data-target="' + i + '" class="current"><a href="javascript:;">●</a></li>');
 	    		else
-	    			$(pointUl).append('<li><a href="javascript:;">●</a></li>');
+	    			point = $('<li data-target="' + i + '"><a href="javascript:;">●</a></li>');
+
+	    		$(pointUl).append(point);
 	    	}
-	    }	
+
+	    	$(pointUl).find('a').on('click', selectPoint);
+	    };	
+	    var selectPoint = function(e){
+	    	var index = $(e.target).parent().attr('data-target');
+	    	index = parseInt(index);
+
+	    	wizard.current = index;
+
+	    	var selectedArr = [];
+			if (index == 1){
+				selectedArr = [index, index + 1, index + 2];
+			}else if(index == wizard.imgNums){
+				selectedArr = [index - 2, index - 1, index];
+			}else{
+				selectedArr = [index - 1, index, index + 1];
+			}	    			
+
+			showImg(selectedArr);
+	    };
+
+	    var showImg = function(points = []){
+	    	$.each(wizard.imgs, function(index, imgLi){
+	    		var identifier = $(imgLi).attr('id');
+	    		if($.inArray(index, points))
+	    			$('#' + ).show();
+	    		else
+	    			$(imgLi).hide();
+
+	    		if(index == wizard.current){
+	    			if(!$(imgLi).hasClass('current'))
+	    				$(imgLi).addClass('current')
+	    		}else{
+	    			if($(imgLi).hasClass('current'))
+	    				$(imgLi).removeClass('current')
+	    		}
+	    	});
+
+	    };
+	    var initImg = function(){
+			showImg([0,1,2]);
+	    };
 
 		var init = function(){
-			wizard.imgs = $(wizard).find('div').find('li');
-			wizard.imgNums = wizard.imgNums.length;
-		}
+			wizard.imgs = $(wizard).find('div.minPic').find('li');
+			wizard.imgNums = wizard.imgs.length;
+
+			initSelectPoints();
+			initImg();
+		};
 
 		init();
 	}
--- js/scroll.js
+++ js/scroll.js
@@ -1,40 +1,3 @@
-/*
-== malihu jquery custom scrollbar plugin == 
-Version: 3.1.5 
-Plugin URI: http://manos.malihu.gr/jquery-custom-content-scroller 
-Author: malihu
-Author URI: http://manos.malihu.gr
-License: MIT License (MIT)
-*/
-
-/*
-Copyright Manos Malihutsakis (email: manos@malihu.gr)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
-/*
-The code below is fairly long, fully commented and should be normally used in development. 
-For production, use either the minified jquery.mCustomScrollbar.min.js script or 
-the production-ready jquery.mCustomScrollbar.concat.min.js which contains the plugin 
-and dependencies (minified). 
-*/
 
 (function(factory){
 	if(typeof define==="function" && define.amd){
@@ -46,19 +9,19 @@ and dependencies (minified).
 	}
 }(function($){
 (function(init){
-	var _rjs=typeof define==="function" && define.amd, /* RequireJS */
-		_njs=typeof module !== "undefined" && module.exports, /* NodeJS */
-		_dlp=("https:"==document.location.protocol) ? "https:" : "http:", /* location protocol */
-		_url="cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js";
-	if(!_rjs){
-		if(_njs){
-			require("jquery-mousewheel")($);
-		}else{
-			/* load jquery-mousewheel plugin (via CDN) if it's not present or not loaded via RequireJS 
-			(works when mCustomScrollbar fn is called on window load) */
-			$.event.special.mousewheel || $("head").append(decodeURI("%3Cscript src="+_dlp+"//"+_url+"%3E%3C/script%3E"));
-		}
-	}
+	//var _rjs=typeof define==="function" && define.amd, /* RequireJS */
+	//	_njs=typeof module !== "undefined" && module.exports, /* NodeJS */
+	//	_dlp=("https:"==document.location.protocol) ? "https:" : "http:", /* location protocol */
+	//	_url="./jquery.mousewheel.min.js";
+	//if(!_rjs){
+	//	if(_njs){
+	//		require("jquery-mousewheel")($);
+	//	}else{
+	//		/* load jquery-mousewheel plugin (via CDN) if it's not present or not loaded via RequireJS 
+	//		(works when mCustomScrollbar fn is called on window load) */
+	//		$.event.special.mousewheel || $("head").append(decodeURI("%3Cscript src="+_dlp+"//"+_url+"%3E%3C/script%3E"));
+	//	}
+	//}
 	init();
 }(function(){
 	
