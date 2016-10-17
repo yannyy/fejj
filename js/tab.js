637274cee702b94925ca99f855fb757efa828a2c
yannyy <yangyan4406@126.com>
2016-10-17 15:11:32 +0800

    tab modify

--- js/tab.js
+++ js/tab.js
@@ -2,20 +2,20 @@
 	var tab = {
 		init: function(target){
 			//初始化事件绑定
-			$('div.tabDiv').find('li').bind('click', tab.select);
-			$.each($('div.tabDiv').find('li'), function(i, li){
-				$(li).find('i').bind('click', function(e){
-					$(li).find('a').trigger('click');
+			$('div.tabDiv').find('a').bind('click', tab.select);
+			$.each($('div.tabDiv').find('a'), function(i, link){
+				$(link).find('i').bind('click', function(e){
+					$(e.target).parent().trigger('click');
 					e.stopPropagation();
 					return false;
 				});
-				$(li).find('span').bind('click', function(e){
-					$(li).find('a').trigger('click');
+				$(link).find('span').bind('click', function(e){
+					$(e.target).parent().trigger('click');
 					e.stopPropagation();
 				});
 			});
-			$('div.tabDiv').find('li').find('i').bind('click', function(){});
-			$('div.tabDiv').find('li').find('span').bind('click', tab.select);
+			//$('div.tabDiv').find('li').find('i').bind('click', function(){});
+			//$('div.tabDiv').find('li').find('span').bind('click', tab.select);
 			$('div.tabContainer').each(function(i, tab){
 				if(i == 0)
 					$(tab).show();
@@ -28,6 +28,11 @@
 			var $clickTaget = e.target;
 			if ($clickTaget.tagName != 'a' && $clickTaget.tagName != 'A')
 				return false;
+
+			if($clickTaget.href != 'javascript:;' && $clickTaget.href != '' && $clickTaget.href != 'undifined' && $clickTaget.href != null){
+				location.href = $clickTaget.href;
+				return;
+			}
 
 			var tabObj = $($clickTaget).parent();
 
