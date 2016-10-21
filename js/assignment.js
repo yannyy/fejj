8420adb594f6769f4d101af421604ff5b639f642
yannyy <yangyan4406@126.com>
2016-10-21 20:02:13 +0800

    assignment widget

--- js/assignment.js
+++ js/assignment.js
@@ -5,7 +5,7 @@
 		var assignment = {
 			data:[],
 			init:function(){
-				$(widget).find('div.ms-button').find('button').on('click', assignment.add);
+				$(widget).find('div.ms-button').find('a').on('click', assignment.add);
 			},
 			add:function(){
 				var src  = $(widget).find('select.src').val();;
@@ -31,20 +31,26 @@
 
 				var icon = $('<i class="fa fa-long-arrow-right"></i>');
 
-				var button = $('<button class="delete" type="button">×</button>')
-				button.on('click', assignment.remove)
+				var span = $('<span class="actionInnerTd"></span>');
+				var link = $('<a></a>');
+				var iconMinus = $('<i class="zmdi zmdi-minus-circle" ></i>');
+				link.append(iconMinus);
+				iconMinus.on('click', assignment.remove);
+				span.append(link);
 
 				li.append(srcLabel);
 				li.append(icon);
 				li.append(destLabel);
-				li.append(button);
+				li.append(span);
 				$list.append(li);	
 				assignment.data.push({src:src, dest:dest});
-				console.log(assignment.data);
 			},
 			remove: function(e){
+				if(e.target.tagName != 'I')
+					return false;
+
 				var target = e.target;
-				var li = $(target).parent();
+				var li = $(target).parent().parent().parent();
 
 				var src  = $(li).find('label.src').text();
 				var dest = $(li).find('label.dest').text();
