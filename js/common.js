f16bc0bf6835dfa3db9a4c1a28f3f2139f392501
yannyy <yangyan4406@126.com>
2016-10-27 12:46:44 +0800

    alerMsg

--- js/common.js
+++ js/common.js
@@ -34,3 +34,51 @@ function hidePopup(target){
 	$(target).parent().hide();
 	$(target).parent().next('div.popupBackdrop').remove();
 }
+function alertMsg(options={}){
+	var title            = options.title || '警告';
+	var msg              = options.msg || '您的输入不合法';
+	var callbackFn       = options.callbackFn || function(){};
+
+	var fade             = $('<div class="popup fade" style="display:block ;">')
+	var popup            = $('<div class="popupDiv">');
+	var content          = $('<div class="popupContent">');	
+
+	var contentHeader    = $('<div class="contentHeader">');
+	var dismissBtn       = $(' <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>');
+	dismissBtn.on('click', function(){
+		fade.remove();
+	});
+	contentHeader.append(dismissBtn);
+
+	var headerTitle      = $('<h4></h4>'); 
+	headerTitle.text(title);
+	contentHeader.append(headerTitle);
+
+	var alertMiddle      = $('<div class="alertDesc alertDesc-info"></div>');
+	var contentBody      = $('<div class="contentBody">');
+	var contentTable     = $('<table class="table-input">');
+	var contentTableBody = $('<tbody><tr><td style="font-size:15px">'+msg+'</td></tr></tbody>');
+
+	contentTable.append(contentTableBody);
+	contentBody.append(contentTable);
+
+	var contentFooter    = $('<div class="contentFooter">');
+	var cancelButton     = $('<button class="btn btn-light">取消</button>');
+	var subButton        = $('<button class="btn btn-color  m-r-10">确定</button>');
+	cancelButton.on('click', function(){
+		fade.remove();
+	});
+	subButton.on('click', function(){
+		callbackFn();
+	});
+	contentFooter.append(cancelButton);
+	contentFooter.append(subButton);
+
+	content.append(contentHeader);
+	content.append(alertMiddle);
+	content.append(contentBody);
+	content.append(contentFooter);
+	popup.append(content);
+	fade.append(popup);
+	$('div.contentDiv').append(fade);
+}
