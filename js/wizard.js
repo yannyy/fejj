1c01f4a57511df7b39df3b659a38e666b7e3af08
yannyy <yangyan4406@126.com>
2016-12-06 17:56:26 +0800

    wizard onPrev

--- js/wizard.js
+++ js/wizard.js
@@ -1,49 +1,61 @@
 (function($){
 	$.fn.wizard = function(options){
 		var wizard = this;
+
+		options = options || {};
+
+		wizard.onNextCallback = options.onNext || function(){return true;};
+        wizard.onPrevCallback = options.onPrev || function(){return true;};
+
 		var steps= $(wizard).find('div.wizardStep');
 		wizard.$steps = steps;	
 		wizard.$current = 0;
 		wizard.$prev = $(wizard).find('button.wizardPrev');
 		wizard.$next = $(wizard).find('button.wizardNext');
-		if(options != null && options.onNext != null)
-			wizard.onNextCallback = options.onNext;
 
 
 		var init = function(){
-			$.each(wizard.$steps, function(i, step){
-				if(i == wizard.$current)
-					$(step).show();
-				else
-					$(step).hide();
-			})
+			wizard.$current	= 0;
+			initWizard();
+		};
 
-			if(wizard.$current == 0)
-				wizard.$prev.attr('disabled', true);
-			else
-				wizard.$prev.attr('disabled', false);
+		var initWizard = function() {
+            $.each(wizard.$steps, function(i, step){
+                if(i == wizard.$current)
+                    $(step).show();
+                else
+                    $(step).hide();
+            })
 
-			if(wizard.$current == (wizard.$steps.length - 1))
-				wizard.$next.text('完成');
-			else{
-				wizard.$next.text('下一步');
-				wizard.$next.attr('disabled', false);
-			}
+            if(wizard.$current == 0)
+                wizard.$prev.attr('disabled', true);
+            else
+                wizard.$prev.attr('disabled', false);
 
-			var progress = Math.round((wizard.$current + 1)*100/wizard.$steps.length) + '%';
-			$(wizard).find('div.wizardProgress').css('width', progress);
+            if(wizard.$current == (wizard.$steps.length - 1))
+                wizard.$next.text('完成');
+            else{
+                wizard.$next.text('下一步');
+                wizard.$next.attr('disabled', false);
+            }
+
+            var progress = Math.round((wizard.$current + 1)*100/wizard.$steps.length) + '%';
+            $(wizard).find('div.wizardProgress').css('width', progress);
 		}
 
 		wizard.finish = function(){
 			wizard.trigger('finish', null, this);	
-		}
+		};
 
 		var onPrev = function(e){
+            if(false == wizard.onPrevCallback())
+                return false;
+
 			if(wizard.$current > 0)
 				wizard.$current --;
 
-			init();
-		}
+			initWizard();
+		};
 
 		var onNext = function(e){
 			if(false == wizard.onNextCallback())
@@ -51,11 +63,11 @@
 			
 			if(wizard.$current < (wizard.$steps.length - 1)){
 				wizard.$current ++;
-				init();
+				initWizard();
 			}else if(wizard.$current == (wizard.$steps.length - 1)){
 				wizard.finish();
 			}
-		}
+		};
 
 		wizard.$prev.bind('click', onPrev);
 		wizard.$next.bind('click', onNext);
