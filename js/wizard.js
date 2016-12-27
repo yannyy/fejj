006c00bc185fced90a7cece4bef354f33d3175f6
yannyy <yangyan4406@126.com>
2016-12-27 15:17:01 +0800

    add hidden steps in wizard

--- js/wizard.js
+++ js/wizard.js
@@ -4,6 +4,7 @@
 
 		options = options || {};
         wizard.$current = options.current || 0;
+        wizard.hiddenSteps = options.hiddenSteps || [];
 
 		wizard.onNextCallback = options.onNext || function(){return true;};
         wizard.onPrevCallback = options.onPrev || function(){return true;};
@@ -27,18 +28,26 @@
 
 		var initWizard = function() {
             $.each(wizard.$steps, function(i, step){
-                if(i == wizard.$current)
-                    $(step).show();
+                if(i == wizard.$current) {
+                    if ($.inArray(i, wizard.hiddenSteps) >= 0) {
+                        $(step).hide();
+                        if(i == wizard.$current) {
+                            wizard.$current ++;
+                        }
+                    } else {
+                        $(step).show();
+                    }
+                }
                 else
                     $(step).hide();
             });
 
-            if(wizard.$current == 0)
+            if(!hasPrev(wizard.$current))
                 wizard.$prev.attr('disabled', true);
             else
                 wizard.$prev.attr('disabled', false);
 
-            if(wizard.$current == (wizard.$steps.length - 1))
+            if(!hasNext(wizard.$current))
                 wizard.$next.text('完成');
             else{
                 wizard.$next.text('下一步');
@@ -47,11 +56,39 @@
 
             var progress = Math.round((wizard.$current + 1)*100/wizard.$steps.length) + '%';
             $(wizard).find('div.wizardProgress').css('width', progress);
-		}
+		};
 
 		wizard.finish = function(){
 			wizard.trigger('finish', null, this);	
 		};
+
+		var hasPrev = function(index){
+            var exists = false;
+            for(var i = 0; i < index; i ++) {
+                if ($.inArray(i, wizard.hiddenSteps) >= 0) {
+                    exists = true;
+                    break;
+                }
+            }
+            if(exists)
+                return exists;
+
+            return index != 0;
+		};
+
+        var hasNext = function(index){
+            var exists = false;
+            for(var i = index; i < wizard.$steps.length; i ++) {
+                if ($.inArray(i, wizard.hiddenSteps) >= 0) {
+                    exists = true;
+                    break;
+                }
+            }
+            if(exists)
+                return exists;
+
+            return index != wizard.$steps.length - 1;
+        };
 
 		var onPrev = function(e){
             if(false == wizard.onPrevCallback())
